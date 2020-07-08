bd9831658381b48c47a38d020737ede7ea609e39
Tobias Oberstein <tobias.oberstein@crossbario.com>
2020-07-08 17:19:04 +0200

    fix ipv6

--- vpc.tf
+++ vpc.tf
@@ -23,13 +23,12 @@ resource "aws_vpc" "crossbarfx_vpc" {
     }
 }
 
-# https://www.terraform.io/docs/providers/aws/r/subnet.html
 
+# https://www.terraform.io/docs/providers/aws/r/subnet.html
 resource "aws_subnet" "crossbarfx_vpc_master" {
     vpc_id                              = aws_vpc.crossbarfx_vpc.id
     cidr_block                          = "10.0.10.0/24"
     map_public_ip_on_launch             = "true"
-    assign_ipv6_address_on_creation     = "true"
     availability_zone                   = var.AWS_AZ[0]
 
     tags = {
@@ -74,7 +73,6 @@ resource "aws_subnet" "crossbarfx_vpc_public1" {
     vpc_id                              = aws_vpc.crossbarfx_vpc.id
     cidr_block                          = "10.0.1.0/24"
     map_public_ip_on_launch             = "true"
-    assign_ipv6_address_on_creation     = "true"
     availability_zone                   = var.AWS_AZ[0]
 
     tags = {
@@ -86,7 +84,6 @@ resource "aws_subnet" "crossbarfx_vpc_public2" {
     vpc_id                              = aws_vpc.crossbarfx_vpc.id
     cidr_block                          = "10.0.2.0/24"
     map_public_ip_on_launch             = "true"
-    assign_ipv6_address_on_creation     = "true"
     availability_zone                   = var.AWS_AZ[1]
 
     tags = {
@@ -98,7 +95,6 @@ resource "aws_subnet" "crossbarfx_vpc_public3" {
     vpc_id                              = aws_vpc.crossbarfx_vpc.id
     cidr_block                          = "10.0.3.0/24"
     map_public_ip_on_launch             = "true"
-    assign_ipv6_address_on_creation     = "true"
     availability_zone                   = var.AWS_AZ[2]
 
     tags = {
@@ -139,6 +135,7 @@ resource "aws_subnet" "crossbarfx_vpc_router3" {
     }
 }
 
+
 # https://www.terraform.io/docs/providers/aws/r/internet_gateway.html
 resource "aws_internet_gateway" "crossbarfx_vpc_gw" {
     vpc_id = aws_vpc.crossbarfx_vpc.id
@@ -147,6 +144,7 @@ resource "aws_internet_gateway" "crossbarfx_vpc_gw" {
         Name = "crossbarfx_vpc_gw"
     }
 }
+
 
 # https://www.terraform.io/docs/providers/aws/r/route_table.html
 resource "aws_route_table" "crossbarfx_vpc_rtb" {
