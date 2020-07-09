0f0b8aeeb11952c29a9069f00e36d63583542f95
Tobias Oberstein <tobias.oberstein@crossbario.com>
2020-07-09 19:46:59 +0200

    sync new

--- aws-s3.tf
+++ aws-s3.tf
@@ -1,5 +1,8 @@
 # Copyright (c) Crossbar.io Technologies GmbH. Licensed under GPL 3.0.
 
+#
+# web
+#
 resource "aws_cloudfront_origin_access_identity" "crossbarfx-web" {
     comment = "crossbarfx-web"
 }
@@ -37,6 +40,7 @@ resource "aws_s3_bucket" "crossbarfx-web" {
 
     logging {
         target_bucket = aws_s3_bucket.crossbarfx-weblog.id
+        target_prefix = "web/"
     }
 
     tags    = {
@@ -60,24 +64,84 @@ resource "aws_s3_bucket_public_access_block" "public-access-crossbarfx-web" {
 }
 
 
+#
+# download
+#
+resource "aws_cloudfront_origin_access_identity" "crossbarfx-download" {
+    comment = "crossbarfx-download"
+}
+
+data "aws_iam_policy_document" "read-crossbarfx-download-bucket" {
+    statement {
+        actions   = ["s3:GetObject"]
+        resources = ["${aws_s3_bucket.crossbarfx-download.arn}/*"]
+
+        principals {
+            type        = "AWS"
+            identifiers = [aws_cloudfront_origin_access_identity.crossbarfx-download.iam_arn]
+        }
+    }
+
+    statement {
+        actions   = ["s3:ListBucket"]
+        resources = [aws_s3_bucket.crossbarfx-download.arn]
+
+        principals {
+            type        = "AWS"
+            identifiers = [aws_cloudfront_origin_access_identity.crossbarfx-download.iam_arn]
+        }
+    }
+}
+
 resource "aws_s3_bucket" "crossbarfx-download" {
     bucket  = var.domain-download-bucket
-    acl     = "public"
+    acl    = "public-read"
+
+    website {
+        index_document = "index.html"
+    }
+
+    logging {
+        target_bucket = aws_s3_bucket.crossbarfx-weblog.id
+        target_prefix = "download/"
+    }
+
     tags    = {
         Name = "crossbarfx-download"
     }
 }
 
+resource "aws_s3_bucket_policy" "read-crossbarfx-download" {
+    bucket = aws_s3_bucket.crossbarfx-download.id
 
+    policy = data.aws_iam_policy_document.read-crossbarfx-download-bucket.json
+}
+
+resource "aws_s3_bucket_public_access_block" "public-access-crossbarfx-download" {
+    bucket = aws_s3_bucket.crossbarfx-download.id
+
+    block_public_acls       = true
+    block_public_policy     = true
+    ignore_public_acls      = true
+    restrict_public_buckets = false
+}
+
+
+#
+# weblog
+#
 resource "aws_s3_bucket" "crossbarfx-weblog" {
     bucket  = var.domain-weblog-bucket
-    acl     = "private"
+    acl     = "log-delivery-write"
     tags    = {
         Name = "crossbarfx-weblog"
     }
 }
 
 
+#
+# backup
+#
 resource "aws_s3_bucket" "crossbarfx-backup" {
     bucket  = var.domain-backup-bucket
     acl     = "private"
